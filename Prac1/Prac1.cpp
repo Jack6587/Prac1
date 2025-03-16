b7277951f33516fe9e6db47fdbc3fc6ec18ac62b
klejt002 <klejt002@mymail.unisa.edu.au>
2025-03-16 10:57:19 +1030

    Activity 2 complete

--- Prac1/Prac1.cpp
+++ Prac1/Prac1.cpp
@@ -1,35 +1,35 @@
 ﻿#include <iostream>
 /* function to read number from standard input (keyboard) */ int getNumber();
 /* function to calculate the sum of two numbers */
-int calculateSum(int, int);
+int calculateSum(int, int, int*);
 /* function to display details to the screen */
 void displayDetails();
 int main() {
 	int num1 = 0; /* first number to be entered by user */
+	int* num1Pointer = &num1;
 	int num2 = 0; /* second number to be entered by user */
+	int* num2Pointer = &num2;
 	int sum = 0; /* stores sum of two numbers */
 	displayDetails();
 	/* prompt for and read first number */
 	std::cout << "Please enter first number : ";
-	num1 = getNumber();
+	num1 = getNumber(num1Pointer);
 	/* prompt for and read second number */
 	std::cout << "Please enter second number: ";
-	num2 = getNumber();
+	num2 = getNumber(num2Pointer);
 	/* add two numbers together and display to screen */
-	sum = calculateSum(num1, num2);
+	sum = calculateSum(num1, num2, &sum);
 	std::cout << std::endl << "\nSum of " << num1 << " and " << num2 << " is: " <<
 		sum << std::endl << std::endl;
 	return 0;
 }
 /* function to read number from standard input (keyboard) */
-int getNumber() {
-	int num;
-	std::cin >> num;
-	return num;
+void getNumber(int *num) {
+	std::cin >> *num;
 }
 /* function to calculate the sum of two numbers */
-int calculateSum(int number1, int number2) {
-	return number1 + number2;
+int calculateSum(int num1, int num2, int* sum) {
+	*sum = number1 + number2;
 }
 /* function to display details to the screen */
 void displayDetails() {

ed93dab6db5cc97b75fef783bd3085d0e2e61264
klejt002 <klejt002@mymail.unisa.edu.au>
2025-03-16 10:32:25 +1030

    Activity 1 complete

--- Prac1/Prac1.cpp
+++ Prac1/Prac1.cpp
@@ -10,26 +10,40 @@ void function1(int);
 void function2(int*);
 int main() {
 	int number = 10;
+	int* numberPtr = &number;
 	/* C1. display the contents of number to the screen */
+	std::cout << "Number equals to " << number << std::endl;
 	/* C2. display the address of variable number to the screen */
+	std::cout << "The address of 'number' is " << &number << std::endl;
 	/* C3. call function1 */
+	function1(number);
 	/* C4. display the contents of number to the screen */
-
+	std::cout << "After calling 'function1', 'number' is now equal to " << number << std::endl;
 	/* C5. call function2 */
+	function2(numberPtr);
 	/* C6. display the contents of number to the screen */
+	std::cout << "After calling 'function2', 'number' is now equal to " << number << std::endl;
 	return 0;
 }
 void function1(int number)
 {
 	/* C7. increment number by one */
+	number += 1;
 	/* C8. display the contents of number to the screen (should be 11) */
+	std::cout << "In 'function1', the value equals to " << number << std::endl;
 	/* C9. display the address of number to the screen */
+	std::cout << "In 'function1', the address of 'number' equals to " << &number << std::endl;
+
 }
 void function2(int* numberPtr)
 {
 	/* C10. display the address of number to the screen (should be the same
 	address as you displayed first in main) */
+	std::cout << "In 'function2', the address of 'number' equals to " << numberPtr << std::endl;
 	/* C11. display the value that the pointer numberPtr points to */
+	std::cout << "In 'function2', the address of 'number' equals to " << *numberPtr << std::endl;
 	/* C12. increment the value stored in number */
+	*numberPtr += 1;
 	/* C13. display the contents of number to the screen */
+	std::cout << "In 'function2', the value of 'number' now equals to " << *numberPtr << std::endl;
 }
